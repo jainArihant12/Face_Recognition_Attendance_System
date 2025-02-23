7379ab23c497a05fb3af41e6ee19b5d2c404ec36
jainArihant12 <arihantjainknp@gmail.com>
2025-02-23 15:38:39 +0530

    adding attendance.py

---
+++ attendance.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as numpy
+
+
+
+face_classifier = cv2.CascadeClassifier('har_face.xml')
+Model = cv2.face.LBPHFaceRecognizer_create()
+Model.read("lbph_classifier.yml")
+
+def face_detector(img, size = 0.5):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+    if faces is():
+        return img,[]
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200,200))
+
+    return img,roi
+
